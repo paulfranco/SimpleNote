19622b7ba3b14e8ee91f553b40112e74ec148010
Paul Franco <paulfranco@me.com>
2020-02-20 17:42:43 -0800

    Initial commit. Created core module

---
+++ app/src/main/java/co/paulfran/paulfranco/simplenote/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package co.paulfran.paulfranco.simplenote.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.paulfran.paulfranco.simplenote.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
