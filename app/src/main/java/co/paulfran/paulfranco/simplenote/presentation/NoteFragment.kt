dcf845a397f102c9c1cecb61015b2e1b6ad98ed8
Paul Franco <paulfranco@me.com>
2020-02-20 18:57:21 -0800

    created ListFragment and NoteFragment

---
+++ app/src/main/java/co/paulfran/paulfranco/simplenote/presentation/NoteFragment.kt
@@ -0,0 +1,24 @@
+package co.paulfran.paulfranco.simplenote.presentation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import co.paulfran.paulfranco.simplenote.R
+
+
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+
+}
